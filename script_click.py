825aabe52a91be0076325a3b86eeee163863dd4b
Johan Rocheteau <johan.rocheteau@hotmail.fr>
2024-05-01 16:14:10 +0200

    Ajout de descriptions

--- script_click.py
+++ script_click.py
@@ -164,6 +164,26 @@ def up_to_bdd(df_ajout, if_exists):
     engine.dispose()
 
 
+def echo_success(message):
+    """Decore pour le succes du programme click."""
+    click.echo(
+        click.style(
+            message.replace("\n                     ", ""),
+            fg="green",
+        )
+    )
+
+
+def echo_failure(message):
+    """Décore en cas d'échéc du programme click."""
+    click.echo(
+        click.style(
+            message.replace("\n                     ", ""),
+            fg="red",
+        )
+    )
+
+
 # 4 : Utilisation de la routine de récupération des données via click :
 
 
@@ -171,30 +191,51 @@ def up_to_bdd(df_ajout, if_exists):
 @click.option(
     "--full",
     is_flag=True,
-    required=True,
-    help="Option de récupération de toutes les donnes.",
+    help="""Option de récupération des donnes depuis le début de la sonde
+    avec une réinitialisation de la table.""",
 )
 @click.option(
     "--update",
     is_flag=True,
-    required=True,
-    help="Option de mise à jour des données.",
+    help="Option de mise à jour et d'ajout des données à la table.",
 )
 def main(full, update):
     """Permet à la fonction click de choisir entre récupérer toutes les données
     ou updater les nouvelles données."""
+
+    if not (full or update):
+        raise click.UsageError(
+            """Vous devez spécifier une des options:
+                               --full ou --update."""
+        )
+
     if full:
+        echo_success(
+            """Le script de récupération totale des données
+                     a été lancé avec succés."""
+        )
         first_day_station, if_exists_bdd = start_station()
         end_api = today_ts()
         df_news = one_day_data(first_day_station, end_api)
         up_to_bdd(df_news, if_exists_bdd)
+        echo_success("Le script s'est exécuté avec succès.")
 
     if update:
+        echo_success(
+            """Le script de mise à jour des données
+                     a été lancé avec succès."""
+        )
         last_ts, if_exists_bdd = last_ts_bdd()
         end_api = today_ts()
         df_news = one_day_data(last_ts, end_api)
         up_to_bdd(df_news, if_exists_bdd)
+        echo_success("Le script s'est exécuté avec succès.")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except click.UsageError as e:
+        echo_failure(f"Erreur d'utilisation : {e}")
+    except click.Abort:
+        echo_failure("L'opération a été interrompue.")
